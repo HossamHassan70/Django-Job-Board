bf512332b0f958dbc33f874200b3ddd3be0e1d44
Hossam Hassan <hossam.h.a.hafez@gmail.com>
2025-08-01 16:08:11 +0300

    add apps

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # My Apps
+    'job',
+    'blog',
+    'contact',
+    'accounts'
 ]
 
 MIDDLEWARE = [
